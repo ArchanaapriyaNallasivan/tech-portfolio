aa8d3e06583502553485817cdcc3b6f38febbbf4
Archanaapriya Nallasivan <37690300+ArchanaapriyaNallasivan@users.noreply.github.com>
2020-10-02 15:15:04 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const siteConfig = {
     tagline: 'Aspiring Junior Software developer',
     profilePhoto: 'img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    baseUrl: '/portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
